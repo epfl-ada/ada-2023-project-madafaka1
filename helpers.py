4ef5528b97794136607da822c187ff32974b1608
Paul Luc Mc Intyre <paul.mcintyre@epfl.ch>
2023-11-15 11:06:55 +0100

    reformat docs as doxygen

--- helpers.py
+++ helpers.py
@@ -3,6 +3,18 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 def convert_and_extract_date(df):
+    """
+    Convert and extract date-related features from the DataFrame.
+
+    Parameters:
+    - df (pd.DataFrame): Input DataFrame containing the 'Movie release date' and 'Movie runtime' columns.
+
+    Returns:
+    pd.DataFrame: Updated DataFrame with added 'Month' and 'Year' columns.
+
+    Example:
+    >>> df = convert_and_extract_date(input_df)
+    """
     # Convert 'Movie release date' to datetime
     df['Movie release date'] = pd.to_datetime(df['Movie release date'])
     # Extract month and year from the release date
@@ -11,7 +23,21 @@ def convert_and_extract_date(df):
     return df
 
 def create_length_bins(df, min_sample_size=25):
-    # Create bins for movie length with a step of 15 minutes up to 300 minutes
+    """
+    Create bins for movie length with a step of 15 minutes up to 300 minutes and aggregate data based on the bins.
+
+    Parameters:
+    - df (pd.DataFrame): Input DataFrame with 'Movie runtime' column.
+    - min_sample_size (int): Minimum sample size for valid bins.
+
+    Returns:
+    pd.DataFrame: Filtered DataFrame with added 'Length Bin' column.
+    pd.DataFrame: Aggregated data based on the length bins.
+
+    Example:
+    >>> filtered_df, grouped_data = create_length_bins(input_df)
+    """
+    # Create bins for movie length 
     bins = list(range(0, 315, 15))
     # Assign movies to bins based on Movie Length
     df['Length Bin'] = pd.cut(df['Movie runtime'], bins=bins, right=False)
@@ -28,6 +54,15 @@ def create_length_bins(df, min_sample_size=25):
     return filtered_movies, grouped_data
 
 def plot_movie_duration_metrics(grouped_data):
+    """
+    Plot various movie duration metrics.
+
+    Parameters:
+    - grouped_data (pd.DataFrame): Aggregated data based on movie length bins.
+
+    Example:
+    >>> plot_movie_duration_metrics(aggregated_data)
+    """
     plt.figure(figsize=(18, 6))
     # Plotting the median box office revenue per movie duration
     plt.subplot(1, 3, 1)
@@ -58,7 +93,16 @@ def plot_movie_duration_metrics(grouped_data):
     plt.show()
 
 def explore_month_release_relationship(df, month_order):
-    # Explore the relationship between movie month release and box office revenue
+    """
+    Explore the relationship between movie month release and various metrics.
+
+    Parameters:
+    - df (pd.DataFrame): Input DataFrame with 'Month' column.
+    - month_order (list): List of month names in the desired order.
+
+    Example:
+    >>> explore_month_release_relationship(input_df, ['Jan', 'Feb', ..., 'Dec'])
+    """
 
     # Map the numeric month values to month names
     df['Month'] = list(map(lambda x: month_order[x - 1], df['Month']))
